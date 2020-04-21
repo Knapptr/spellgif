b65f3d9d27d52dcd33ac7da5cf829b97408938af
Tyler Knapp <Knapptr@gmail.com>
2020-04-20 17:00:12 -0700

    Updates

---
+++ precache-manifest.fd4ac5838ecd736af4513805e42e85c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788d90527627c0f2202079da9cfe9e4e",
+    "url": "/spellgif/index.html"
+  },
+  {
+    "revision": "5b99ebf2f25571daec26",
+    "url": "/spellgif/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "963a6e8cef113f5881b8",
+    "url": "/spellgif/static/css/main.5f94cbff.chunk.css"
+  },
+  {
+    "revision": "5b99ebf2f25571daec26",
+    "url": "/spellgif/static/js/2.fd5d29e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spellgif/static/js/2.fd5d29e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963a6e8cef113f5881b8",
+    "url": "/spellgif/static/js/main.fd919f95.chunk.js"
+  },
+  {
+    "revision": "80bfe0c09d2567d1f114",
+    "url": "/spellgif/static/js/runtime-main.3363b76f.js"
+  }
+]);
